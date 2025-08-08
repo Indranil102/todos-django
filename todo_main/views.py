066c80342cccf0765de155995b642d95fd973d78
Indranil102e <indranilsamanta2003@gmail.com>
2025-08-08 16:54:21 +0530

    the button is adding data from the backend

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,7 +3,7 @@ from todo.models import Task
 
 # Create your views here.
 def home(request):
-    tasks=Task.objects.filter(is_completed=False)
-    print(tasks)
+    tasks=Task.objects.filter(is_completed=False).order_by('-updated_at')
+   
     context={'tasks':tasks}
     return render(request,"home.html",context) 
